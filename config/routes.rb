d1e6a0a5e86b8fe9660be46ad0369b7d2ea84734
Precious Akams <preciousakams@yahoo.com>
2022-08-22 21:40:29 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
+  get 'users' => 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'posts' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
 end
