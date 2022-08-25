6fc2c037695015c624a47b308de492659b498e1c
kamzzy <preciousakams@yahoo.com>
2022-08-26 00:10:15 +0100

    work in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  def show; end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id)
+  end
 end
