723bb18283541e0ba6649f49c117a3c9333a59f5
kamzzy <preciousakams@yahoo.com>
2022-08-26 14:59:56 +0100

    user and post view completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.where(user_id: params[:user_id])
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
   end
 end
