fcc6495d6f742a18f26af584359d2fd16b406c29
Precious Akams <preciousakams@yahoo.com>
2022-09-02 22:11:42 +0100

    controllers updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     @posts = Post.where(user_id: params[:user_id])
     @user = User.find(params[:user_id])
