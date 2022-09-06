fdeb2e9ebbadcb542424bc2c96e6155b9b585bf6
kamzzy <preciousakams@yahoo.com>
2022-09-06 11:44:39 +0100

    devise controllers exposed

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      devise_for :users, controllers: { 
+        registrations: 'api/v1/users/registrations',
+        sessions: 'api/v1/users/sessions' }
       resources :users do
         resources :posts do
           resources :comments
