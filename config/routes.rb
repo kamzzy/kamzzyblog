4f47106aa3e74f68262f213575d55dd5ce1b90e7
kamzzy <preciousakams@yahoo.com>
2022-09-08 16:34:55 +0100

    documentation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :roles
   devise_for :users
   scope '/admin' do
